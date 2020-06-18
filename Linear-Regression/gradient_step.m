569be56015f0865034836751e55ad4ee6fcdb6f1
Niceno <bojan.niceno.scientist@gmail.com>
2020-06-18 19:06:03 +0200

    Removed depreciated feature.

--- Linear-Regression/gradient_step.m
+++ Linear-Regression/gradient_step.m
@@ -1,5 +1,5 @@
 %===============================================================================
-  function [theta] = \
+  function [theta] = ...
     gradient_step(x, y, theta, alpha, lambda)
 %-------------------------------------------------------------------------------
 % Function performs one step of gradient descent for theta parameters.
